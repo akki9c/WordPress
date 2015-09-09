e8f23dc8c66658771f0fc0a4b5fd14baf35d9a79
akash ujjwal <akki9c@gmail.com>
2015-09-09 10:20:47 +0530

    Change log
    CSV_Import v1.0.1 bug-fixes:
    1. Secure it by anonymous entry.
    2. Automatic remove the uploaded CSV file after import.

--- Import CSV Into Database/csv_import.php
+++ Import CSV Into Database/csv_import.php
@@ -1,32 +1,30 @@
-<?php 
+<?php
 
-/*
-Author: AKASH
-URI: github.com/akki9c
+
+/*Author: AKASH
+  URI: github.com/akki9c
+ 
 */
 
 ?>
-
-
-
-
 <?php
-$conn = mysql_connect("localhost", "db_username", "db_pass");
+
+
+$conn = mysql_connect("localhost", "qosyco_wor5584", "fl2AfBAgXx2R");
 
 if( $conn === false ) {
 		die( 'unable to connect');
 	}
 
 
-if (!mysql_select_db('db_name', $conn)) {
+if (!mysql_select_db('qosyco_wor5584', $conn)) {
 		echo 'Could not select database';
 		exit;
 	}
 	
 	
 $file_name = $_POST['filename'];	
-$filename = "wp-content/....Your File path.../$file_name";
-/*For Example: $filename = "wp-content/uploads/csvfiles/$file_name";*/
+$filename = "wp-content/uploads/csvfiles/$file_name";
 
 /* function to create csv file in array */
 
@@ -47,30 +45,34 @@ $filename = "wp-content/....Your File path.../$file_name";
         }
         fclose($handle);
     }
-/*print_r ($data);*/
-$count=count($data);
-/*echo "$count";
+/*print_r ($data);
+
+
+
+echo "$count";
 $each_data = $data[0];
 echo $each_data[product_name];*/
+$count=count($data);
 
 
 
 
 /*query for Count the number of fields */
-$result = mysql_query(("SELECT COUNT(*) as total FROM table_name"), $conn); 
+$result = mysql_query(("SELECT COUNT(*) as total FROM wp_pswe_komper_field"), $conn); 
 					
 $total=mysql_fetch_assoc($result);
 $total_field = $total['total'];		
-/*echo $total_field;*/
+// echo $total_field;
+
 
 
 /*query for minimum value of field Id*/
-$result = mysql_query(("SELECT MIN(id) as Minimum FROM table_name"), $conn); 
+$result = mysql_query(("SELECT MIN(id) as Minimum FROM wp_pswe_komper_field"), $conn); 
 					
 $total=mysql_fetch_assoc($result);
 $minimum_field_id = $total['Minimum'];
 /*query for maximum value of product id*/
-$result = mysql_query(("SELECT Max(id) as maximum FROM table_name"), $conn); 
+$result = mysql_query(("SELECT Max(id) as maximum FROM wp_pswe_komper_product"), $conn); 
 					
 $total=mysql_fetch_assoc($result);
 $maximum_product_id = $total['maximum'];
@@ -89,8 +91,8 @@ if($maximum_product_id == NULL)
 				
 	
 				$each_data = $data[$p-1];
-				$tsql = "INSERT INTO table_name (id,product_name,product_image,product_summary)
-							VALUES ($p,'$each_data[product_name]', '$each_data[product_image]','$each_data[product_summary]')"; 
+				$tsql = "INSERT INTO wp_pswe_komper_product (id,product_name,product_image,product_summary)
+							VALUES ($p,'$each_data[product_name]', '$each_data[product_image]', '$each_data[product_summary]')"; 
 					
 					if( mysql_query($tsql, $conn))
 						{
@@ -106,7 +108,7 @@ if($maximum_product_id == NULL)
 					while($i <= $total_field) 
 						{
 							$field = "field_"."$i";
-							$tsql = "INSERT INTO table_name (field_values,product_id,field_id)
+							$tsql = "INSERT INTO wp_pswe_komper_value (field_values,product_id,field_id)
 									VALUES ('$each_data[$field]',$p,$f)"; 
 									
 							if( mysql_query($tsql, $conn))
@@ -134,8 +136,8 @@ while($p<=($count+$maximum_product_id-1))
 {
 		$each_data = $data[($p-$maximum_product_id)-1];
 	
-	$tsql = "INSERT INTO table_name (id,product_name,product_image,product_summary)
-							VALUES ($p,'$each_data[product_name]', '$each_data[product_image]','$each_data[product_summary]')"; 
+	$tsql = "INSERT INTO wp_pswe_komper_product (id,product_name,product_image,product_summary)
+							VALUES ($p,'$each_data[product_name]', '$each_data[product_image]', '$each_data[product_summary]')"; 
 					
 					if( mysql_query($tsql, $conn))
 						{
@@ -151,7 +153,7 @@ while($p<=($count+$maximum_product_id-1))
 					while($i <= $total_field) 
 						{
 							$field = "field_"."$i";
-							$tsql = "INSERT INTO table_name (field_values,product_id,field_id)
+							$tsql = "INSERT INTO wp_pswe_komper_value (field_values,product_id,field_id)
 									VALUES ('$each_data[$field]',$p,$f)"; 
 									
 							if( mysql_query($tsql, $conn))
@@ -167,17 +169,21 @@ while($p<=($count+$maximum_product_id-1))
 							}
 					
 					}
-
 					$p++;
 			}
 }
+
 echo "<h1>All Records Are successfully Imported.</h1><br />";
 echo "Number of row inserted="." ".$count." "."<br />";
 echo "Number of column inserted=".($total_field+3)."<br />";
 
-echo "<a href='http://example.com/'><- Go Back To Home</a><br />";
-echo "<a href='http://example.com/insertpage.php'>Click Here To Create Bulk Of Page</a><br /><strong>(NOTE:Don't Click twice.)</strong>";
+/*echo "<a href='http://brokernotes.co/'><- Go Back To Home</a><br />";*/
+echo "<a href='http://brokernotes.co/insert_broker_page.php?ak=ak'>Click Here To Create Bulk Of Page</a><br /><strong>(NOTE:Don't Click twice.)</strong>";
 
 ?>
 
+<?php
 
+unlink($filename);
+
+?>
